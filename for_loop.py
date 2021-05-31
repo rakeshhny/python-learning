ea6774c329fbaf50c1501d2a1ca648574a513d7d
Rakesh Kadulkar <rakesh.kadulkar@gmail.com>
2021-05-31 20:40:22 +0530

    added program to test for loop

---
+++ for_loop.py
@@ -0,0 +1,9 @@
+# For loops in Python
+
+# Take the begin and ending values from the command prompt as an input.
+
+begin = int(input("Enter the begin number."))
+ending = int(input("Enter the ending number."))
+
+for i in range(begin,ending):
+    print("i =",i,"\n")
