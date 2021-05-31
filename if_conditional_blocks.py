cd5f909be77fd1ae0634436ca1db7fd5e9f53424
Rakesh Kadulkar <rakesh.kadulkar@gmail.com>
2021-05-31 20:47:34 +0530

    if conditional block test

---
+++ if_conditional_blocks.py
@@ -0,0 +1,16 @@
+# Program to test the usage of if...elif...else conditional blocks.
+
+# Progra to check if the input number is even or odd. Returns invalid input for 0.
+
+num = int(input("Input the number : "))
+
+if ( num == 0 ):
+    print("Invalid Input")
+elif ( num % 2 == 0 ):
+    print("Even Number")
+else:
+    print("Odd number")
+        
+
+    
+    
