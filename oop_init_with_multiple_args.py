5ee63f1aa4f8391406ad4ed1d3cbbc4e50571306
Rakesh Kadulkar <rakesh.kadulkar@gmail.com>
2021-06-03 21:00:03 +0530

    Added init function with multiple arguments.

---
+++ oop_init_with_multiple_args.py
@@ -0,0 +1,12 @@
+# OOP in Python
+
+# Init function with multiple arguments
+
+class Test:
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+
+
+t1 = Test(3,4)
+print("a =",t1.a,"and b =",t1.b)
