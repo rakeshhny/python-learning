d7920b0b1764c38b64cefbbb6f0885d2f18e81c2
Rakesh Kadulkar <rakesh.kadulkar@morningstar.com>
2021-08-09 21:51:57 +0530

    Sorting employees in the ascending order of name and descending order of salary

--- Assignment14_1.py
+++ Assignment14_1.py
@@ -20,5 +20,16 @@ for i in range(1, 3):
     emp = Employee.acceptEmpData()
     employees.append(emp)
 
+# Sort the list of employees in the ascending order of name
+employees.sort(key=lambda x: x.name, reverse=False)
+
+print("Employees in the ascending order of name")
+for emp in employees:
+    emp.showEmpData()
+
+# sort the list of employees in descending order or salary
+employees.sort(key=lambda x: x.salary, reverse=True)
+
+print("Employees in the descending  order of salary")
 for emp in employees:
     emp.showEmpData()
